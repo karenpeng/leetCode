681ea522d3300e9b713cc29186e0dcfa16762cce
Karen <karenpenglabs@gmail.com>
2015-10-02 19:36:58 -0400

    有向图

---
+++ data_structure/courseScheduleII.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {number[]}
+ */
+var findOrder = function(numCourses, prerequisites) {
+    if(prerequisites.length === 0) return
+    
+    var graph = []
+    var deepth = []
+    var queue = []
+    var result = []
+    
+    for(var i = 0; i < numCourses; i++){
+        graph[i] = []
+        deepth[i] = 0
+    }
+    
+    prerequisites.forEach(function(arr){
+        graph[arr[1]].push(arr[0])
+        deepth[arr[0]] ++
+    })
+    
+    for(var i = 0; i < numCourses; i++){
+        if(deepth[i] === 0) queue.push(i)
+    }
+    
+    while(queue.length > 0){
+        var c = queue.shift()
+        result.push(c)
+        graph[c].forEach(function(cc){
+            deepth[cc]--
+            if(deepth[cc] === 0){
+                queue.push(cc)
+            }
+        })
+    }
+    
+    if(result.length !== numCourses) result = []
+    
+    return result
+};
