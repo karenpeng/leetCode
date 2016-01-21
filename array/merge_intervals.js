12fa5e9b82f409cae86247e670e7b1660224e741
ᶘ ᵒᴥᵒᶅ Karen <itismekarenp@gmail.com>
2016-01-20 16:45:43 -0800

    Update merge_intervals.js

--- array/merge_intervals.js
+++ array/merge_intervals.js
@@ -7,6 +7,41 @@
  */
 /**
  * @param {Interval[]} intervals
+ * @param {Interval} newInterval
+ * @return {Interval[]}
+ */
+var insert = function(intervals, newInterval) {
+    var bucket = [];
+    var insert = false;
+    intervals.forEach(function(item){
+       if(insert) bucket.push(item);
+       else{
+           if(item.end < newInterval.start){
+               bucket.push(item);
+           }else if(item.start > newInterval.end){
+               bucket.push(newInterval);
+               bucket.push(item);
+               insert = true;
+           }else{
+                newInterval.start = Math.min(item.start, newInterval.start);
+                newInterval.end = Math.max(item.end, newInterval.end);
+           }
+       }
+    });
+    if(!insert) bucket.push(newInterval);
+    return bucket;
+};
+
+
+/**
+ * Definition for an interval.
+ * function Interval(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ * }
+ */
+/**
+ * @param {Interval[]} intervals
  * @return {Interval[]}
  */
 var merge = function(intervals) {
