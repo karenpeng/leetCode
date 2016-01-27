4271754dea190a3bec246749750b94d42024fe71
ᶘ ᵒᴥᵒᶅ Karen <itismekarenp@gmail.com>
2016-01-27 13:19:05 -0800

    Update path_sumII.js

--- DFS/path_sumII.js
+++ DFS/path_sumII.js
@@ -1,56 +1,51 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
- * @param {TreeNode} root
- * @param {number} sum
- * @return {number[][]}
- */
-var pathSum = function(root, sum) {
-    if(root === null) return []
-    var list = []
-    var result = []
-    helper(root, list, result, sum)
-    return result
-};
-
-function helper(root, list, result, sum){
-    
-    list.push(root.val)
-    
-    if(root.left === null && root.right === null){
-        if(sumup(list) === sum){
-            result.push(deepCopy(list))
-        }
-    }
-    
-    if(root.left !== null){
-        helper(root.left, list, result, sum)
-        list.pop()
-    }
-    
-    if(root.right !== null){
-        helper(root.right, list, result, sum)
-        list.pop()
-    }
+function Node(val){
+  this.val = val
+  this.left = null
+  this.right = null
 }
 
-function sumup(arr){
-    var sum = 0
-    arr.forEach(function(e){
-        sum += e
-    })
-    return sum
+function main(root, sum){
+  var result = []
+  helper(root, sum, 0, [], result)
+  return result
+}
+
+function helper(root, sum, mySum, list, result){
+  
+  mySum += root.val
+  list.push(root.val)
+  
+  if(root.left === null && root.right === null && sum === mySum){
+    result.push(deepCopy(list))
+  }
+  
+  if(root.left !== null){
+    helper(root.left, sum, mySum, list, result)
+    list.pop()
+  }
+  
+  if(root.right !== null){
+    helper(root.right, sum, mySum, list, result)
+    list.pop()
+  }
+  
 }
 
 function deepCopy(arr){
-    var _arr = []
-    arr.forEach(function(e){
-        _arr.push(e)
-    })
-    return _arr
+  return arr.map(function(item){
+    return item
+  })
 }
+
+var a = new Node(1)
+var b = new Node(2)
+var c = new Node(3)
+var d = new Node(1)
+
+a.left = b
+a.right = c
+b.left = d
+
+console.log(main(a, 4)) //=> [[1,2,1], [1,3]]
+
+
