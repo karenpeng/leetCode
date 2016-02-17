76eef7f2d0c53d3baee0f0924ae7691e98358721
ᶘ ᵒᴥᵒᶅ Karen <itismekarenp@gmail.com>
2016-02-17 15:20:21 -0800

    Update Minimum_Size_Subarray_Sum.js

--- array/Minimum_Size_Subarray_Sum.js
+++ array/Minimum_Size_Subarray_Sum.js
@@ -24,3 +24,28 @@ var minSubArrayLen = function(s, nums) {
     return l === Infinity ? 0 : l
 };
 
+/**
+ * @param {number} s
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(s, nums) {
+    if(nums.length === 0) return 0
+    var i = 0
+    var start = 0
+    var end = 0
+    var l = Infinity
+    var sum = 0
+    while(end < nums.length){
+        while(end < nums.length && sum < s){
+            sum += nums[end]
+            end++
+        }
+        while(sum >= s){
+            l = Math.min(l, end - start)
+            sum -= nums[start]
+            start++
+        }
+    }
+    return l === Infinity ? 0 : l
+};
